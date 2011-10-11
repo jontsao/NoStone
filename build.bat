2048e5350aa0f06fdf4dbc770099b5d4b2e2b113
hrichardlee <hrichardlee@gmail.com>
2011-10-10 20:07:52 -0700

    Added a build script. Only works on my machine

---
+++ build.bat
@@ -0,0 +1,4 @@
+C:\Users\rilee\AppData\Local\Google\Chrome\Application\chrome.exe --pack-extension=c:\users\rilee\documents\projects\nostone\source --pack-extension-key=c:\users\rilee\documents\projects\nostone\nostone.pem --no-message-box
+
+del NoStone.crx
+move source.crx NoStone.crx
